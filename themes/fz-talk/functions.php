7a6aff284cef828e9ade42d4a22da1b6c9e25ae6
Faison Zutavern <fzutavern@my.wctc.edu>
2015-09-12 00:26:04 -0500

    Proper initialization of the child theme

--- themes/fz-talk/functions.php
+++ themes/fz-talk/functions.php
@@ -1,15 +1,28 @@
 <?php
-/**
- * Theme functions for the FZ Talk Theme.
- */
-
-namespace FZ_Talk_Theme;
 
 /**
- * Enqueues the parent theme style
+ * FZ Talk functions and definitions
+ *
+ * When using a child theme (see http://codex.wordpress.org/Theme_Development and
+ * http://codex.wordpress.org/Child_Themes), you can override certain functions
+ * (those wrapped in a function_exists() call) by defining them first in your child theme's
+ * functions.php file. The child theme's functions.php file is included before the parent
+ * theme's file, so the child theme functions would be used.
+ *
+ * @package FZ Talk
+ * @since 0.1.0
  */
-function fz_talk_enqueue_styles() {
-	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-}
 
-add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\fz_talk_enqueue_styles' );
+// Useful global constants
+define( 'FZTALK_VERSION',      '0.1.0' );
+define( 'FZTALK_URL',          get_stylesheet_directory_uri() );
+define( 'FZTALK_PATH',         get_stylesheet_directory() . '/' );
+define( 'FZTALK_INC',          FZTALK_PATH . 'includes/' );
+
+// Include compartmentalized functions
+require_once FZTALK_INC . 'functions/core.php';
+
+// Include lib classes
+
+// Run the setup functions
+FZ_Talk\Core\setup();
---
+++ themes/fz-talk/includes/functions/core.php
@@ -0,0 +1,91 @@
+<?php
+namespace FZ_Talk\Core;
+
+/**
+ * Set up theme defaults and register supported WordPress features.
+ *
+ * @since 0.1.0
+ *
+ * @uses add_action()
+ *
+ * @return void.
+ */
+function setup() {
+	add_action( 'after_setup_theme',  __NAMESPACE__ . '\i18n' );
+	add_action( 'wp_head',            __NAMESPACE__ . '\header_meta' );
+	add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\scripts' );
+	add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\styles' );
+}
+
+/**
+ * Makes WP Theme available for translation.
+ *
+ * Translations can be added to the /lang directory.
+ * If you're building a theme based on WP Theme, use a find and replace
+ * to change 'wptheme' to the name of your theme in all template files.
+ *
+ * @uses load_theme_textdomain() For translation/localization support.
+ *
+ * @since 0.1.0
+ *
+ * @return void.
+ */
+function i18n() {
+	load_theme_textdomain( 'fztalk', FZTALK_PATH . '/languages' );
+ }
+
+/**
+ * Enqueue scripts for front-end.
+ *
+ * @uses wp_enqueue_script() to load front end scripts.
+ *
+ * @since 0.1.0
+ *
+ * @return void.
+ */
+function scripts( $debug = false ) {
+	$min = ( $debug || defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
+
+	wp_enqueue_script(
+		'fztalk',
+		FZTALK_TEMPLATE_URL . "/assets/js/fz-talk{$min}.js",
+		array(),
+		FZTALK_VERSION,
+		true
+	);
+}
+
+/**
+ * Enqueue styles for front-end.
+ *
+ * @uses wp_enqueue_style() to load front end styles.
+ *
+ * @since 0.1.0
+ *
+ * @return void.
+ */
+function styles( $debug = false ) {
+	$min = ( $debug || defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
+
+	wp_enqueue_style(
+		'fztalk',
+		FZTALK_URL . "/assets/css/fz-talk{$min}.css",
+		array(),
+		FZTALK_VERSION
+	);
+}
+
+/**
+ * Add humans.txt to the <head> element.
+ *
+ * @uses apply_filters()
+ *
+ * @since 0.1.0
+ *
+ * @return void.
+ */
+function header_meta() {
+	$humans = '<link type="text/plain" rel="author" href="' . FZTALK_TEMPLATE_URL . '/humans.txt" />';
+
+	echo apply_filters( 'fztalk_humans', $humans );
+}
