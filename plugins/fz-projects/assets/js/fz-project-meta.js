ea3bb0dabf457d1ca9d38a2f1f07187e92c1e9a0
Faison Zutavern <fzutavern@my.wctc.edu>
2015-09-13 11:38:00 -0500

    Running grunt on the past commits

---
+++ plugins/fz-projects/assets/js/fz-project-meta.js
@@ -0,0 +1,34 @@
+/*! FZ Projects - v0.1.0
+ * https://github.com/Faison/ThisIsSoMetaTalk
+ * Copyright (c) 2015; * Licensed GPLv2+ */
+( function ( $ ) {
+	'use strict';
+
+	function init_handlers() {
+		$( '.js-fz-dropdown-list' ).on( 'click', '.js-fz-remove', remove_list_item );
+
+		$( '.js-fz-meta-field' ).on( 'click', '.js-fz-dropdown-list-add', add_list_item );
+	}
+
+	function remove_list_item( e ) {
+		var $this      = $( e.target),
+			$parent_li = $this.closest( 'li' );
+
+		$parent_li.remove();
+	}
+
+	$( document ).ready( function() {
+		init_handlers();
+	} );
+
+	function add_list_item( e ) {
+		var $target_list = $( e.target ).siblings( '.js-fz-dropdown-list'),
+			$list_item   = $target_list.children().first(),
+			$new_item    = $list_item.clone();
+
+		$new_item.find( 'option:selected' ).removeAttr( 'selected' );
+
+		$target_list.append( $new_item );
+	}
+
+} )( jQuery );
